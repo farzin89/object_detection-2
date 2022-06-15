edabe87d4df4b3680c4855940cc827b4b03fe37b
farzin89 <farzin_ahmadi90@yahoo.com>
2022-06-15 11:10:16 +0200

    detect contour and create rectangle wirh Background Subtraction

--- object_detection_ 1/vehicle_detection 1.py
+++ object_detection_ 1/vehicle_detection 1.py
@@ -3,22 +3,52 @@ import numpy as np
 import matplotlib as plt
 
 cap = cv2.VideoCapture("video.mp4")
+#cap = cv2.VideoCapture(1)
 
-backgroundobject = cv2.createBackgroundSubtractorMOG2(detectShadows = False)
+kernel = None
+backgroundobject = cv2.createBackgroundSubtractorMOG2( detectShadows = True)
 
-while(1) :
+while True :
     ret,frame = cap.read()
     if not ret :
         break
 
     fgmask = backgroundobject.apply(frame)
-    real_part = cv2.bitwise_and(frame,frame,mask = fgmask)
-    fgmask_3 = cv2.cvtColor(fgmask,cv2.COLOR_GRAY2BGR)
 
-    stacked = np.hstack((fgmask_3,frame,real_part))
-    cv2.imshow("All three",cv2.resize(stacked,None,fx = 0.65,fy=0.65))
+    #perform thresholding to get ride of the shadows.
+    _,fgmask = cv2.threshold(fgmask,250,255,cv2.THRESH_BINARY)
 
-    k = cv2.waitKey(30) & 0xFF
+    # apply some morphological operations to make sure you have a good mask
+    fgmask = cv2.erode(fgmask,kernel,iterations = 1)
+    fgmask = cv2.dilate(fgmask,kernel,iterations = 2)
+
+    # detect contours in the frame
+    contours,_ = cv2.findContours(fgmask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    frameCopy = frame.copy()
+
+    # loop over each contour found in the frame.
+    for cnt in contours:
+
+        #make sure the contour area is somewhat higher than some threshold to make sure its a car and not some noise.
+        if cv2.contourArea(cnt) > 400:
+            x,y,width,height = cv2.boundingRect(cnt)
+            cv2.rectangle(frameCopy,(x,y),(x+width,y+height),(0,0,255),2)
+            # write car detection near the bounding box drawn
+            cv2.putText(frameCopy,"Car Detected",(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,0.3,(0,255,0),1,cv2.LINE_AA)
+
+
+    # extract the foreground from the frame using the segmented mask.
+    foregroundpart = cv2.bitwise_and(frame,frame,mask = fgmask)
+
+    # Stack the original frame, extracted foreground,and annotated frame.
+    stacked = np.hstack((frame,foregroundpart,frameCopy))
+
+
+
+    cv2.imshow("All three",cv2.resize(stacked,None,fx = 0.5,fy=0.5))
+    cv2.imshow("Clean Mask",fgmask)
+
+    k = cv2.waitKey(10) & 0xFF
     if k == 27:
         break
 
